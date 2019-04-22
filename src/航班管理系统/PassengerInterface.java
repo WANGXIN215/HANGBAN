a3858c4b6defef889a2b6fc8109f8d192dd919c2
huangdingluan <316943766@qq.com>
2019-04-22 23:03:52 +0800

    3171104153 航班管理系统 王鑫 第一次上传

---
+++ src/航班管理系统/PassengerInterface.java
@@ -0,0 +1,24 @@
+package 航班管理系统;
+
+public interface PassengerInterface {
+	/**
+	 * 获取旅客姓名
+	 * @return 返回姓名
+	 */
+	public String getName();
+	/**
+	 * 获取预定号
+	 * @return 返回预定号
+	 */
+	public int getBookingNumber();
+	/**
+	 * 获取座位排数
+	 * @return 返回排数
+	 */
+	public int getRow();
+	/**
+	 * 获取座位号
+	 * @return 返回座位号
+	 */
+	public int getSeatPosition();
+}
