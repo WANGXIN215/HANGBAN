bf4da947583ba39360e2b9fd49123ffd0fc7ac20
huangdingluan <316943766@qq.com>
2019-04-22 23:45:08 +0800

    3171104153 王鑫  航班系统  第三次上传

---
+++ src/航班管理系统/FlightInterface.java
@@ -0,0 +1,26 @@
+package 航班管理系统;
+
+/**
+ * 航班接口
+ */
+
+public interface FlightInterface {
+	/**
+	 * 预定航班座位
+	 * @param names 存放旅客姓名的字符串数组
+	 * @return 返回存放预定号的数组
+	 */
+	public int[] reserve(String[] names);
+	/**
+	 * 取消预定座位
+	 * @param bookingNumber 该参数指定要取消的预定号
+	 * @return 返回该座位的预定状态
+	 */
+	public boolean canncel(int bookingNumber);
+	/**
+	 * 获取预定了座位的旅客列表
+	 * @return 返回存放旅客类对象的数组 
+	 */
+	public Passenger[] getPassengerList();
+
+}
